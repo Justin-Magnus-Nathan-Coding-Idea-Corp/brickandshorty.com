c19e8260107f4077c901813c477e73f8b4517ee5
justincockrell1 <1greatnate888@gmail.com>
2021-10-22 18:45:11 -0500

    homepageupdate

---
+++ javascript/test.js
@@ -0,0 +1,16 @@
+const title = document.querySelector("#maintitle");
+
+let posY =0;
+
+// window.setInterval(()=>{
+//     let posX = Math.random()*window.innerWidth;
+//     posY = Math.random()*window.innerHeight;
+//     title.style.left = `${posX}px`;
+//     title.style.top = `${posY}px`;
+// },2000);
+
+
+window.addEventListener("keydown",(e)=>{
+    posY-=10;
+    title.style.top = `${posY}px`;
+});
